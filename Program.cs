6e016b7d18e37e6a87d48ae0d66d7fe0db0cd535
danill26 <49979520+danill26@users.noreply.github.com>
2019-04-25 20:52:11 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projek1
+{
+    class Program
+    {
+        private static int index = 0;
+        
+        static void Main(string[] args)
+        {
+            
+            var menu = new menu(); 
+            
+            List<string> menuItem = new List<string>()
+            {
+                "Menu",
+                "Transaksi",
+                "Exit"
+            };
+            Console.CursorVisible = false;
+            while (true)
+            {
+                string selectedMenuitem = drawMenu(menuItem);
+
+                if (selectedMenuitem == "Menu")
+                {
+                    Console.Clear();
+                    Console.WriteLine("$Bakso: {Name}" + "$15000: {Harga}" + "$true: {Status}");
+                }
+                else if (selectedMenuitem == "Exit")
+                {
+                    Environment.Exit(0);
+                }
+                    
+            }
+        }
+
+        private static string drawMenu(List<string> menuItem)
+        {
+            for (int i = 0; i < menuItem.Count; i++)
+            {
+                if (i == index)
+                {
+                    Console.BackgroundColor = ConsoleColor.Blue;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine(menuItem[i]);
+                }
+                else
+                {
+                    Console.WriteLine(menuItem[i]); 
+                }
+                Console.ResetColor(); 
+            }
+            
+            ConsoleKeyInfo ckey = Console.ReadKey();
+
+            if(ckey.Key == ConsoleKey.DownArrow)
+            {
+                if (index == menuItem.Count - 1)
+                {
+                    //index = 0;
+                } else
+                {
+                    index++;
+                }
+            }
+            else if (ckey.Key == ConsoleKey.UpArrow)
+            {
+                if (index <= 0)
+                {
+                    //index = menuItem.Count - 1;
+                } else
+                {
+                    index--;
+                }
+            } else if (ckey.Key == ConsoleKey.Enter)
+            {
+                return menuItem[index];
+            } else
+            {
+                return "";
+            }
+            Console.Clear();
+            return "";
+
+        }
+    }
+}
---
+++ menu.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projek1
+{
+    public class menu
+    {
+        private List<string> _menu;
+
+        public string Name { get; set; }
+        public int Harga { get; set; }
+        public bool Status { get; set; }
+
+        public menu()
+        {
+            Name = "Bakso";
+            Harga = 15000;
+            Status = true;
+
+            _menu = new List<string>()
+            {
+                "Bakso; 15000; true;",
+                "Siomay; 10000; true;",
+                "Pizza; 35000; true;"
+            };
+        }
+        public string Info()
+        {
+            var info = $"Nama: {Name}" + $"Harga: {Harga}; Status: {Status}";
+            return info;
+        }
+
+        public void Create(string menu)
+        {
+            _menu.Add(menu);
+        }
+
+        public List<string> Read()
+        {
+            return _menu;
+        }
+
+        public void Update(string menu)
+        {
+            _menu.Remove(menu);
+            _menu.Add(menu);
+        }
+        public void Delete(string menu)
+        {
+            _menu.Remove(menu);
+        }
+
+
+
+    }
+}
